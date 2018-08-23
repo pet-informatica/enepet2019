d1c38e0d37d1e28877e492b60a59d621984772bf
hcaula <lhtc@cin.ufpe.br>
2018-08-23 00:19:22 -0300

    Adiciona header

---
+++ src/containers/Home/Header/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="Header">
+                <div className="Header-logo">
+                    Header logo
+                </div>
+                <div className="Header-options">
+                    <div className="Header-option">
+                    </div>
+                    <div className="Header-option">
+                    </div>
+                    <div className="Header-option">
+                    </div>
+                    <div className="Header-option">
+                    </div>
+                    <div className="Header-option">
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import logo from '../../assets/imgs/logo.svg';
+import Header from './Header';
 import './Home.css';
 
-class App extends Component {
+class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <Header></Header>
+        Hi!
       </div>
     );
   }
 }
 
-export default App;
+export default Home;
