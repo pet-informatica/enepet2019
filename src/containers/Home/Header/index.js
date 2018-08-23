d382703442a705b74bec5fcb2a6e4c352ff2bcf2
hcaula <lhtc@cin.ufpe.br>
2018-08-23 00:52:24 -0300

    Checa se tela esta em modo mobile

--- src/containers/Home/Header/index.js
+++ src/containers/Home/Header/index.js
@@ -2,26 +2,37 @@ import React, { Component } from 'react';
 import './Header.css';
 
 class Header extends Component {
+
     render() {
-        return (
-            <div className="Header">
-                <div className="Header-logo">
+
+        console.log(this.props);
+
+        const desktop = (
+            <div className="Header Header-desktop">
+                <div className="Header-logo Header-logo-desktop">
                     Header logo
                 </div>
-                <div className="Header-options">
-                    <div className="Header-option">
+                <div className="Header-options Header-options-desktop">
+                    <div className="Header-option Header-option-desktop">
+                        Header option 1
                     </div>
-                    <div className="Header-option">
+                    <div className="Header-option Header-option-desktop">
+                        Header option 2
                     </div>
-                    <div className="Header-option">
+                    <div className="Header-option Header-option-desktop">
+                        Header option 3
                     </div>
-                    <div className="Header-option">
+                    <div className="Header-option Header-option-desktop">
+                        Header option 4
                     </div>
-                    <div className="Header-option">
+                    <div className="Header-option Header-option-desktop">
+                        Header option 5
                     </div>
                 </div>
             </div>
-        );
+        )
+
+        return desktop;
     }
 }
 
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 import Header from './Header';
 import './Home.css';
 
+const mobileWidth = 500;
+
 class Home extends Component {
-  render() {
-    return (
-      <div>
-          <Header></Header>
-        Hi!
-      </div>
-    );
-  }
+    
+    state = {
+        isMobile: this.checkIfMobile()
+    }
+    
+    componentDidMount() {
+        window.addEventListener('resize', () => {
+            this.setState({ isMobile: this.checkIfMobile() });
+        });
+    }
+
+    checkIfMobile() {
+        return (window.innerWidth < mobileWidth);
+    }
+
+    render() {
+        return (
+            <div className="Home">
+                <Header isMobile={this.state.isMobile}></Header>
+                Hi!
+            </div>
+        );
+    }
 }
 
 export default Home;
