5af5a2aad43f25ad8ef9946cb662b92e3d71a248
hcaula <lhtc@cin.ufpe.br>
2018-08-30 11:58:53 -0300

    Adiciona esqueleto do componente de comissoes

--- src/containers/Home/Commissions/index.js
+++ src/containers/Home/Commissions/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Card from '../../../components/Card';
+import thumbnail from '../../../assets/imgs/thumbnail.svg';
 import './Commissions.css';
 
 class Commissions extends Component {
@@ -10,7 +12,39 @@ class Commissions extends Component {
                     <h1>Comissões</h1>
                 </div>
                 <div className="Commisions-body">
-                    
+                    <Card
+                        title="Attraction 1"
+                        img={thumbnail}
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non eleifend sem, sed pulvinar dui. Vestibulum facilisis, arcu sit amet gravida vulputate."
+                        link="https://google.com"
+                        buttonName="Google"
+                    >
+                    </Card>
+                    <Card
+                        title="Attraction 2"
+                        img={thumbnail}
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non eleifend sem, sed pulvinar dui. Vestibulum facilisis, arcu sit amet gravida vulputate.">
+                    </Card>
+                    <Card
+                        title="Attraction 3"
+                        img={thumbnail}
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non eleifend sem, sed pulvinar dui. Vestibulum facilisis, arcu sit amet gravida vulputate.">
+                    </Card>
+                    <Card
+                        title="Attraction 4"
+                        img={thumbnail}
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non eleifend sem, sed pulvinar dui. Vestibulum facilisis, arcu sit amet gravida vulputat.">
+                    </Card>
+                    <Card
+                        title="Attraction 5"
+                        img={thumbnail}
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non eleifend sem, sed pulvinar dui. Vestibulum facilisis, arcu sit amet gravida vulputate.">
+                    </Card>
+                    <Card
+                        title="Attraction 6"
+                        img={thumbnail}
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non eleifend sem, sed pulvinar dui. Vestibulum facilisis, arcu sit amet gravida vulputate.">
+                    </Card>
                 </div>
             </div>
         );
